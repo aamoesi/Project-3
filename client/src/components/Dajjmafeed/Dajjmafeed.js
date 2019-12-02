66045ff53e516e9d241b8f54e0bb6b5c71840e46
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-12-02 16:53:48 -0500

    Added PokemonClicky game to Dashboard

--- client/src/components/Dajjmafeed/Dajjmafeed.js
+++ client/src/components/Dajjmafeed/Dajjmafeed.js
@@ -1,15 +1,13 @@
-// NOTE: This file will feed brain games from other developers.  More to come.
+// NOTE: This file will feed brain games from DAJJMA!  More to come.
 
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Gamefeed.css";
+// import "./Gamefeed.css";
 
-const Gamefeed = props => (
-    // <div className="game-thumb">
-    //     <a href="https://cdn.htmlgames.com/MergeIt/" target="_blank"><img src="https://www.htmlgames.com/uploaded/thumb500/mergeit500.jpg"></a>
-    // </div>
+const Dajjmafeed = props => (
+
     
-    // Games Produced by Other Developers
+    // Games Produced by DAJJMA!
     <div className="game-thumb">
         <Link to="/games/mergeit">
             <img
@@ -23,7 +21,7 @@ const Gamefeed = props => (
                 src='https://www.htmlgames.com/uploaded/thumb500/1010hex500.jpg'
             />
         </Link>
-        <Link to="/games/mainpower">
+        <Link to="/pokemon-clicky">
             <img
                 style={{ width: 300, height: 200, padding: 15, borderRadius: 25 }}
                 src='https://cdn.htmlgames.com/MainPower/img/icon/image-300x200.jpg'
@@ -32,4 +30,4 @@ const Gamefeed = props => (
     </div>
 );
 
-export default Gamefeed;
+export default Dajjmafeed;
