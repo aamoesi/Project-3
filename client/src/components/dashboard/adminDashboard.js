bed29edfa70bc43d0b24e244ad21a76a767feb71
Dano <danielaherniv@gmail.com>
2019-12-02 11:02:23 -0500

    update

--- client/src/components/dashboard/adminDashboard.js
+++ client/src/components/dashboard/adminDashboard.js
@@ -6,13 +6,18 @@ const adminDashboard = props => {
         <h1>
 
             This will be our ADMIN Dashboard.
+            <br />
 
             Goals for this dashoard:
-
+            <br />
             1. Display users progress with games.
+            <br />
             2. Have area for Admin users to keep notes and track progress.
+            <br />
             3. Administrators comments.
+            <br />
             4. Inoformation about patients.
+            <br />
             5. Information about Administrators.
 
         </h1>
