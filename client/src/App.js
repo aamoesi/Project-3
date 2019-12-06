394f48c7baa6ab2592d9eddb59c361b6d6824e21
Dano <danielaherniv@gmail.com>
2019-12-05 22:39:21 -0500

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-
 import AdminRegister from './components/auth/AdminRegister';
 import AdminLogin from './components/auth/AdminLogin';
-
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import adminDashboard from './components/dashboard/adminDashboard';
@@ -24,76 +21,36 @@ import Mergeit from './components/games/Mergeit';
 import Tentenhex from './components/games/1010hex';
 import Mainpower from './components/games/Mainpower';
 import './App.css';
-
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-
 import './App.css';
-
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
-
-function MainSwitch({ location }) {
-  return (
-    <section className={location.pathname !== "/pokemon-clicky" ? "container" : ""}>
-      <Alert />
-      <Switch>
-        <Route exact path='/Register' component={Register} />
-        <Route exact path='/Login' component={Login} />
-
-        <Route exact path='/AdminRegister' component={AdminRegister} />
-        <Route exact path='/AdminLogin' component={AdminLogin} />
-
-        <Route exact path='/administrators' component={adminSpinner} />
-        <Route exact path='/Dashboard' component={Dashboard} />
-        <Route exact path='/adminDashboard' component={adminDashboard} />
-
-        {/* Games Developed by DAJJMA */}
-        <PrivateRoute exact path='/Game1' component={Game1} />
-        <PrivateRoute exact path='/Game2' component={Tetris} />
-        <PrivateRoute exact path='/pokemon-clicky' component={PokemonClicky} />
-        {/* Games Developed by Other Developers */}
-        <PrivateRoute exact path='/games/mergeit' component={Mergeit} />
-        <PrivateRoute exact path='/games/1010hex' component={Tentenhex} />
-        <PrivateRoute exact path='/games/mainpower' component={Mainpower} />
-      </Switch>
-    </section>
-  )
-};
-
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
-
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
           <Route exact path='/' component={Landing} />
-
-          <Route path = "/" component={MainSwitch} />
-
           <section className='container'>
             <Alert />
             <Switch>
               <Route exact path='/Register' component={Register} />
               <Route exact path='/Login' component={Login} />
-
               <Route exact path='/AdminRegister' component={AdminRegister} />
               <Route exact path='/AdminLogin' component={AdminLogin} />
-
               <Route exact path='/administrators' component={adminSpinner} />
               <PrivateRoute exact path='/Dashboard' component={Dashboard} />
               <PrivateRoute exact path='/adminDashboard' component={adminDashboard} />
-
               <PrivateRoute exact path='/edit-profile' component={EditAdminProfile} />
-
               {/* Games Developed by DAJJMA */}
               <PrivateRoute exact path='/Game1' component={Game1} />
               <PrivateRoute exact path='/Game2' component={Tetris} />
@@ -104,13 +61,9 @@ const App = () => {
               <PrivateRoute exact path='/games/mainpower' component={Mainpower} />
             </Switch>
           </section>
-
         </Fragment>
       </Router>
     </Provider>
   );
 };
-
-
-
 export default App;
